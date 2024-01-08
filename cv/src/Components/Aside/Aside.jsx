b1d6a31ff7ef0db595635c8a0d7f5665cc6cfdfd
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-08 12:07:30 +0300

    del aside css

--- cv/src/Components/Aside/Aside.jsx
+++ cv/src/Components/Aside/Aside.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import AsideSection from './AsideSection/AsideSection'
 import Languages from './Languages/Languages'
 import Skills from './Skills/Skills'
-import styles from './Aside.module.css'
 
 const sectionProps = [
   { title: 'Address', disc: 'Krichev, 213500, Mogilev Region, Belarus' },
