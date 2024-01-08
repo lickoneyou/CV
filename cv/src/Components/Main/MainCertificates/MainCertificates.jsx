b2883a117f554e3a6ed42cfa49d8abbbe970480c
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-08 12:41:13 +0300

    add MainCertificates loader

--- cv/src/Components/Main/MainCertificates/MainCertificates.jsx
+++ cv/src/Components/Main/MainCertificates/MainCertificates.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './MainCertificates.module.css'
 import certificates from '../../../data/certificates'
+import Loader from 'react-js-loader'
 
 const MainCertificates = (props) => {
+  const [loading, setLoading] = useState(true)
+  const counter = useRef(0)
+  const imageLoaded = () => {
+    counter.current += 1
+    if (counter.current >= certificates.length) {
+      setLoading(false)
+    }
+  }
   useEffect(() => {
     props.st('otherApp')
   })
   return (
     <main className={styles.MainCertificates}>
+      <div style={{ display: loading ? 'block' : 'none', margin: '0 auto' }}>
+        <Loader bgColor="#000" />
+      </div>
       {certificates.map((el) => {
         return (
-          <div className={styles.certificateWrapper} key={el.title}>
+          <div
+            className={styles.certificateWrapper}
+            key={el.title}
+            style={{ display: loading ? 'none' : 'block' }}
+          >
             <a href={el.link} target="_blank" rel="noopener noreferrer">
-              <img src={el.img} alt={el.title} />
+              <img onLoad={imageLoaded} src={el.img} alt={el.title} />
               <span className={styles.img__mask}>
                 <b className={styles.title}>{el.title}</b>
               </span>
