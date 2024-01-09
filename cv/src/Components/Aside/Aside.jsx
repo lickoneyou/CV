52be2cbba9db7295cb1cdbcd3c4dd7a69983cb6b
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-01-09 21:04:15 +0300

    add aside adaptive

--- cv/src/Components/Aside/Aside.jsx
+++ cv/src/Components/Aside/Aside.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AsideSection from './AsideSection/AsideSection'
 import Languages from './Languages/Languages'
 import Skills from './Skills/Skills'
+import styles from './Aside.module.css'
 
 const sectionProps = [
   { title: 'Address', disc: 'Krichev, 213500, Mogilev Region, Belarus' },
@@ -11,16 +12,16 @@ const sectionProps = [
 
 const Aside = () => {
   return (
-    <aside>
-      <section>
+    <aside className={styles.aside}>
+      <section className={styles.aboutSection}>
         {sectionProps.map((el) => (
           <AsideSection title={el.title} disc={el.disc} key={el.title} />
         ))}
       </section>
-      <section>
+      <section className={styles.languagesSection}>
         <Languages />
       </section>
-      <section>
+      <section className={styles.skillsSection}>
         <Skills />
       </section>
     </aside>
