25728e31b7a10c95e99b3871522c5455b4411b66
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-22 20:58:18 +0300

    update Profile data

--- src/data/about.js
+++ src/data/about.js
@@ -3,57 +3,83 @@ const profile = [
     title: 'Profile',
     descr: (
       <div>
-        <p>Чего я добился:</p>
+        <p>What I achieved:</p>
         <ul>
-          <li>Создал 𝟏𝟖 сайтов и приложений</li>
-          <li>Создал 𝟑 расширения для браузера</li>
+          <li>Created 𝟏𝟖 websites and applications</li>
+          <li>Created 𝟑 browser extensions</li>
           <li>
-            <a href="https://github.com/bradtraversy/50projects50days">
-              Выполнил 𝟓𝟎 заданий курса 𝟓𝟎 𝐏𝐫𝐨𝐣𝐞𝐜𝐭𝐬 𝟓𝟎 𝐃𝐚𝐲𝐬
+            <a
+              href="https://github.com/bradtraversy/50projects50days"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Completed 𝟓𝟎 course assignments 𝟓𝟎 𝐏𝐫𝐨𝐣𝐞𝐜𝐭𝐬 𝟓𝟎 𝐃𝐚𝐲𝐬
             </a>
           </li>
           <li>
-            <a href="https://rs.school/">Получил 𝟐 сертификата 𝐑𝐒 𝐒𝐜𝐡𝐨𝐨𝐥</a>
+            <a
+              href="https://rs.school/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Received 𝟐 certificate 𝐑𝐒 𝐒𝐜𝐡𝐨𝐨𝐥
+            </a>
           </li>
           <ul>
             <li>
-              <a>
+              <a target="_blank" rel="noopener noreferrer">
                 THE ROLLING SCOPES SCHOOL JS/FE PRE- SCHOOL 2022Q2 (JAVASCRIPT)
               </a>
             </li>
             <li>
-              <a>
+              <a target="_blank" rel="noopener noreferrer">
                 THE ROLLING SCOPES SCHOOL JAVASCRIPT/FRONT- END 2022Q3
                 (JAVASCRIPT)
               </a>
             </li>
           </ul>
           <li>
-            <a href="https://www.freecodecamp.org/">
-              Получил 4 сертификата 𝐟𝐫𝐞𝐞𝐂𝐨𝐝𝐞𝐂𝐚𝐦𝐩
+            <a
+              href="https://www.freecodecamp.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Received 4 certificates 𝐟𝐫𝐞𝐞𝐂𝐨𝐝𝐞𝐂𝐚𝐦𝐩
             </a>
           </li>
           <ul>
             <li>
-              <a>FRONT END DEVELOPMENT LIBRARIES</a>
+              <a target="_blank" rel="noopener noreferrer">
+                FRONT END DEVELOPMENT LIBRARIES
+              </a>
             </li>
             <li>
               <a>JavaScript Algorithms and Data Structures Certification</a>
             </li>
             <li>
-              <a>JavaScript Algorithms and Data Structures (Beta)</a>
+              <a target="_blank" rel="noopener noreferrer">
+                JavaScript Algorithms and Data Structures (Beta)
+              </a>
             </li>
             <li>
-              <a>Responsive Web Design</a>
+              <a target="_blank" rel="noopener noreferrer">
+                Responsive Web Design
+              </a>
             </li>
           </ul>
           <li>
-            <a>Набрал 𝟐𝟓𝟎𝟎+ баллов 𝐂𝐨𝐝𝐞𝐰𝐚𝐫𝐬</a>
+            <a target="_blank" rel="noopener noreferrer">
+              Scored 𝟐𝟓𝟎𝟎+ points 𝐂𝐨𝐝𝐞𝐰𝐚𝐫𝐬
+            </a>
           </li>
           <li>
-            <a href="https://github.com/lickoneyou/Courses">
-              Прошёл 𝟏𝟖 курсов от 𝐁𝐨𝐠𝐝𝐚𝐧 𝐒𝐭𝐚𝐬𝐡𝐜𝐡𝐮𝐤, 𝐢𝐓𝐆𝐢𝐝, 𝐋𝐨𝐟𝐭𝐬𝐜𝐡𝐨𝐨𝐥,
-              𝐏𝐮𝐫𝐩𝐥𝐞𝐒𝐜𝐡𝐨𝐨𝐥 и других
+            <a
+              href="https://github.com/lickoneyou/Courses"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Completed 𝟏𝟖 courses from 𝐁𝐨𝐠𝐝𝐚𝐧 𝐒𝐭𝐚𝐬𝐡𝐜𝐡𝐮𝐤, 𝐢𝐓𝐆𝐢𝐝, 𝐋𝐨𝐟𝐭𝐬𝐜𝐡𝐨𝐨𝐥,
+              𝐏𝐮𝐫𝐩𝐥𝐞𝐒𝐜𝐡𝐨𝐨𝐥 and others
             </a>
           </li>
         </ul>
