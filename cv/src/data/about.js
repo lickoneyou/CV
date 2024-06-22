bc5634bb59d4f61e55fd433a50992c9a92a5b23c
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-22 20:42:44 +0300

    update Profile data

--- cv/src/data/about.js
+++ cv/src/data/about.js
@@ -1,8 +1,64 @@
 const profile = [
   {
     title: 'Profile',
-    descr:
-      "I'm an enthusiastic and detail-oriented Frontend Software Engineer seeking an entry-level position with Company to use my skills in coding, troubleshooting complex problems, and assisting in the timely completion of projects.",
+    descr: (
+      <div>
+        <p>Чего я добился:</p>
+        <ul>
+          <li>Создал 𝟏𝟖 сайтов и приложений</li>
+          <li>Создал 𝟑 расширения для браузера</li>
+          <li>
+            <a href="https://github.com/bradtraversy/50projects50days">
+              Выполнил 𝟓𝟎 заданий курса 𝟓𝟎 𝐏𝐫𝐨𝐣𝐞𝐜𝐭𝐬 𝟓𝟎 𝐃𝐚𝐲𝐬
+            </a>
+          </li>
+          <li>
+            <a href="https://rs.school/">Получил 𝟐 сертификата 𝐑𝐒 𝐒𝐜𝐡𝐨𝐨𝐥</a>
+          </li>
+          <ul>
+            <li>
+              <a>
+                THE ROLLING SCOPES SCHOOL JS/FE PRE- SCHOOL 2022Q2 (JAVASCRIPT)
+              </a>
+            </li>
+            <li>
+              <a>
+                THE ROLLING SCOPES SCHOOL JAVASCRIPT/FRONT- END 2022Q3
+                (JAVASCRIPT)
+              </a>
+            </li>
+          </ul>
+          <li>
+            <a href="https://www.freecodecamp.org/">
+              Получил 4 сертификата 𝐟𝐫𝐞𝐞𝐂𝐨𝐝𝐞𝐂𝐚𝐦𝐩
+            </a>
+          </li>
+          <ul>
+            <li>
+              <a>FRONT END DEVELOPMENT LIBRARIES</a>
+            </li>
+            <li>
+              <a>JavaScript Algorithms and Data Structures Certification</a>
+            </li>
+            <li>
+              <a>JavaScript Algorithms and Data Structures (Beta)</a>
+            </li>
+            <li>
+              <a>Responsive Web Design</a>
+            </li>
+          </ul>
+          <li>
+            <a>Набрал 𝟐𝟓𝟎𝟎+ баллов 𝐂𝐨𝐝𝐞𝐰𝐚𝐫𝐬</a>
+          </li>
+          <li>
+            <a href="https://github.com/lickoneyou/Courses">
+              Прошёл 𝟏𝟖 курсов от 𝐁𝐨𝐠𝐝𝐚𝐧 𝐒𝐭𝐚𝐬𝐡𝐜𝐡𝐮𝐤, 𝐢𝐓𝐆𝐢𝐝, 𝐋𝐨𝐟𝐭𝐬𝐜𝐡𝐨𝐨𝐥,
+              𝐏𝐮𝐫𝐩𝐥𝐞𝐒𝐜𝐡𝐨𝐨𝐥 и других
+            </a>
+          </li>
+        </ul>
+      </div>
+    ),
   },
 ]
 
