786eafa028cc4939a70fce65c9c379f76b7ca871
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-23 12:02:52 +0300

    added ControlledPagination component

---
+++ src/Components/Main/Pagination/ControledPagination.jsx
@@ -0,0 +1,27 @@
+import { Pagination } from '@mantine/core'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+const ControlledPagination = ({ pages }) => {
+  const [activePage, setPage] = useState(1)
+  const dispatch = useDispatch()
+
+  return (
+    <Pagination
+      value={activePage}
+      boundaries={0}
+      onChange={(e) => {
+        setPage(e)
+        dispatch({
+          type: 'CHANGE_PAGE',
+          payload: e,
+        })
+        window.scrollTo(0, 0)
+      }}
+      total={pages}
+      style={{ display: 'flex', justifyContent: 'center' }}
+    />
+  )
+}
+
+export default ControlledPagination
