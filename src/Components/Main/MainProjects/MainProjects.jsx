97baeb218321a51cc6f4dd725bba16d35113faeb
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-23 12:02:41 +0300

    added ControlledPagination

--- src/Components/Main/MainProjects/MainProjects.jsx
+++ src/Components/Main/MainProjects/MainProjects.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useRef, useState } from 'react'
 import styles from './MainProjects.module.css'
 import Loader from 'react-js-loader'
 import projects from '../../../data/projects'
+import ControlledPagination from '../Pagination/ControledPagination'
+import _ from 'lodash'
+import { useSelector } from 'react-redux'
 
 const MainProjects = (props) => {
   const [loading, setLoading] = useState(true)
@@ -16,27 +19,34 @@ const MainProjects = (props) => {
     props.st('otherApp')
   })
 
+  const chunkProjects = _.chunk(projects, 6)
+  const page = useSelector((data) => data.pagesReducer)
+  const myProjects = chunkProjects[page - 1]
+
   return (
-    <main className={styles.MainProjects}>
-      <div style={{ display: loading ? 'block' : 'none', margin: '0 auto' }}>
-        <Loader bgColor="#000" />
+    <main>
+      <div className={styles.MainProjects}>
+        <div style={{ display: loading ? 'block' : 'none', margin: '0 auto' }}>
+          <Loader bgColor="#000" />
+        </div>
+        {myProjects.map((el) => {
+          return (
+            <div
+              className={styles.projectWrapper}
+              key={el.title}
+              style={{ display: loading ? 'none' : 'block' }}
+            >
+              <a href={el.link} target="_blank" rel="noopener noreferrer">
+                <img onLoad={imageLoaded} src={el.img} alt={el.title} />
+                <span className={styles.img__mask}>
+                  <b className={styles.title}>{el.title}</b>
+                </span>
+              </a>
+            </div>
+          )
+        })}
       </div>
-      {projects.map((el) => {
-        return (
-          <div
-            className={styles.projectWrapper}
-            key={el.title}
-            style={{ display: loading ? 'none' : 'block' }}
-          >
-            <a href={el.link} target="_blank" rel="noopener noreferrer">
-              <img onLoad={imageLoaded} src={el.img} alt={el.title} />
-              <span className={styles.img__mask}>
-                <b className={styles.title}>{el.title}</b>
-              </span>
-            </a>
-          </div>
-        )
-      })}
+      <ControlledPagination pages={chunkProjects.length} />
     </main>
   )
 }
