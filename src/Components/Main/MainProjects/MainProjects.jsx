55443fde1deacd0ae861913d686d2e688c0d8851
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-06-23 12:53:39 +0300

    fix MainProjects component

--- src/Components/Main/MainProjects/MainProjects.jsx
+++ src/Components/Main/MainProjects/MainProjects.jsx
@@ -1,20 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import styles from './MainProjects.module.css'
-import Loader from 'react-js-loader'
 import projects from '../../../data/projects'
 import ControlledPagination from '../Pagination/ControledPagination'
 import _ from 'lodash'
 import { useSelector } from 'react-redux'
+import Image from '../Image/Image'
 
 const MainProjects = (props) => {
-  const [loading, setLoading] = useState(true)
-  const counter = useRef(0)
-  const imageLoaded = () => {
-    counter.current += 1
-    if (counter.current >= projects.length) {
-      setLoading(false)
-    }
-  }
   useEffect(() => {
     props.st('otherApp')
   })
@@ -26,21 +18,15 @@ const MainProjects = (props) => {
   return (
     <main>
       <div className={styles.MainProjects}>
-        <div style={{ display: loading ? 'block' : 'none', margin: '0 auto' }}>
-          <Loader bgColor="#000" />
-        </div>
+        
         {myProjects.map((el) => {
           return (
             <div
               className={styles.projectWrapper}
               key={el.title}
-              style={{ display: loading ? 'none' : 'block' }}
             >
               <a href={el.link} target="_blank" rel="noopener noreferrer">
-                <img onLoad={imageLoaded} src={el.img} alt={el.title} />
-                <span className={styles.img__mask}>
-                  <b className={styles.title}>{el.title}</b>
-                </span>
+                <Image src={el.img} alt={el.title}/>
               </a>
             </div>
           )
