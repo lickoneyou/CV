5be51a67c9c3c535bc6e313de0c3c320044aa8f1
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-07-02 15:40:17 +0300

    added CertificateImage component

--- src/reducers/pagesReducer.js
+++ src/reducers/pagesReducer.js
@@ -1,8 +1,12 @@
-export const pagesReducer = (state = 1, action) => {
+export const pagesReducer = (
+  state = { projectsPage: 1, certificatesPage: 1 },
+  action,
+) => {
   switch (action.type) {
-    case 'CHANGE_PAGE':
-      state = action.payload
-      return state
+    case 'CHANGE_PROJECT_PAGE':
+      return { ...state, projectsPage: action.payload }
+    case 'CHANGE_CERTIFICATES_PAGE':
+      return { ...state, certificatesPage: action.payload }
     default:
       return state
   }
